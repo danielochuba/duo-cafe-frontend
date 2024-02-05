c8d76c8ed2c4bcce28d278fa2f0382acb3b492c7
Daniel Ochuba <danielochuba78@gmail.com>
2024-02-06 00:25:55 +0100

    create and style login page

---
+++ src/components/auth/login.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import style from '../../assets/stylesheets/login.module.css'
+import { Link } from 'react-router-dom'
+
+function Login() {
+  return (
+    <div className={`${style.login_container} p-5 text-center`}>
+        <form className={`${style.login_form}  p-5 flex flex-col sm:w-1/2 md:w-1/3 space-y-10`}>
+        <h1 className={`${style.form_title}`}>Login</h1>
+            <label for='email'>
+            <input className='border rounded-md p-2 w-full' id='email' type="text" name="name" placeholder='Email..'/>
+            </label>
+  
+            <label for='password'>
+              <input className='border rounded-md p-2 w-full' type="password" name="password" placeholder='Password' />
+            </label>
+            
+           
+            <input className='border w-1/3 rounded-lg p-2 m-auto hover:bg-slate-800 hover:text-white font-bold text-lg' type="submit" value="Submit" />
+            
+        </form>
+      
+    </div>
+  )
+}
+
+export default Login;
