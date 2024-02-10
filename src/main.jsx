2c972238551dd3287fd798375626cd658239f166
Daniel Ochuba <danielochuba78@gmail.com>
2024-02-11 00:00:20 +0100

    remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import './index.css'
 import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
 )
