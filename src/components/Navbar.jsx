d404fbf39be5c9802e334ef7998a07fc7e30b2e9
Daniel Ochuba <danielochuba78@gmail.com>
2024-03-22 09:36:19 +0100

    create Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from './../assets/stylesheets/navbar.module.css';
+function Navbar() {
+  return (
+    <>
+    <nav className="bg-gray-800 h-16">
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+            
+            </div>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
