759e33f64bf2709f8f91dd7f4bbcec2d9299b82c
Daniel Ochuba <danielochuba78@gmail.com>
2024-03-25 22:14:14 +0100

    modify layout for offers

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -3,16 +3,20 @@ import HeroImage from '../assets/images/hero.png';
 
 const Offer = () => {
 
-
   return (
-    <div className="w-[9rem] h-[9rem] rounded-tl-[50px] rounded-tr-[10px] rounded-br-[50px] rounded-bl-[10px]"
+
+    <div className="ml-5 mr-5 w-[120px] h-[9rem] rounded-tl-[50px] rounded-tr-[10px] rounded-br-[50px] rounded-bl-[10px]"
     style={{
         backgroundImage: `url(${HeroImage})`,
         // opacity: 0.7
       }}>
+
      Offer
+     
     </div>
+
   )
+
 }
 
 export default Offer;
